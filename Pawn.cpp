69b8b86712b5d80f54fa32088cc08bf118ebffcb
yanivb390 <yaniv112233@gmail.com>
2017-01-01 18:23:32 +0200

    Update Pawn.cpp

--- Pawn.cpp
+++ Pawn.cpp
@@ -40,40 +40,40 @@ bool Pawn::squareCheck(Pieces* arr[][8], string dst)
 {
 	bool ans = false;
 	int y1 = int(dst[1] - '0'), y2 = int(dst[3] - '0'), x1 = int(dst[0] - 'a') + 1, x2 = int(dst[2] - 'a') + 1;
-	
-	if (_side[0] == 'W' && y1 == 2 && y2 == y1 + 2 && x1 == x2) // White pawn special move up
+	// Indexes are as follows: x1 and y1 are the source indexes, x2 and y2 are the destination indexes
+	if (_side[0] == 'W' && y1 == 2 && y2 == y1 + 2 && x1 == x2) // White pawn special move up, if they are white and in the second row from the bottom
 	{
-		if (!(arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken()))
+		if (!(arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken())) // Check if destination is not taken
 		{
 			return ans;
 		}
 	}
 
-	if (_side[0] == 'B' && y1 == 7 && y2 == y1 - 2 && x1 == x2) // Black pawn special move down
+	if (_side[0] == 'B' && y1 == 7 && y2 == y1 - 2 && x1 == x2) // Black pawn special move down, if they are black and in the second row from the top
 	{
-		if (!(arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken()))
+		if (!(arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken())) // Check if destination is not taken
 		{
 			return ans;
 		}
 	}
 
 	if (_side[0] == 'W' && x1 == x2 && y1 == y2 - 1 && !(arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken())) // White move one up and its free
-	{
+	{ // Check if white and if only one step up and if it is not taken
 		return ans;
 	}
 
 	if (_side[0] == 'B' && x1 == x2 && y1 == y2 + 1 && !(arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken())) // Black move one down and its free
-	{
+	{ // Check if black and if only one step down and if it is not taken
 		return ans;
 	}
 
 	if ((_side[0] == 'W' && x1 == x2 + 1 && y1 == y2 - 1) /* UP LEFT */ && (arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken()) || (_side[0] == 'W' && x1 == x2 - 1 && y1 == y2 - 1) /* UP RIGHT */ && (arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken())) // White eating
-	{
+	{ // Check if white and for each side check if taken, if yes then eat
 		return ans;
 	}
 	
 	if ((_side[0] == 'B' && x1 == x2 + 1 && y1 == y2 + 1) /* DOWN LEFT */ && (arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken()) || (_side[0] == 'B' && x1 == x2 - 1 && y1 == y2 + 1) /* DOWN RIGHT */ && (arr[int('8' - dst[3])][int(dst[2] - 'a')]->getTaken())) // Black eating
-	{
+	{ // Check if black and for each side check if taken, if yes then eat
 		return ans;
 	}
 
